b88b1175330a9700cfc058575d2d820c7efbb69f
fabricekra2812 <fabricekra28@gmail.com>
2026-08-13 15:23:12 +0000

    Update start.sh

--- gateway/start.sh
+++ gateway/start.sh
@@ -3,19 +3,19 @@
 echo "Waiting for backend services..."
 
 # On attend que users_service soit joignable
-while ! nc -z users-service.microservices_vault.svc.cluster.local 8001; do
+while ! nc -z users-service.microservices-vault.svc.cluster.local 8001; do
   echo "Waiting for users_service..."
   sleep 2
 done
 
 # On attend que products_service soit joignable
-while ! nc -z products-service.microservices_vault.svc.cluster.local 8002; do
+while ! nc -z products-service.microservices-vault.svc.cluster.local 8002; do
   echo "Waiting for products_service..."
   sleep 2
 done
 
 # On attend que orders_service soit joignable
-while ! nc -z orders-service.microservices_vault.svc.cluster.local 8003; do
+while ! nc -z orders-service.microservices-vault.svc.cluster.local 8003; do
   echo "Waiting for orders_service..."
   sleep 2
 done
